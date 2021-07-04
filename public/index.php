072ff814881664499526682fac9db2e99da0a7e0
GilbertoASJ <alvesgilberto84@gmail.com>
2021-07-04 17:33:23 -0300

    :sparkles: Updatind file

--- public/index.php
+++ public/index.php
@@ -1,6 +1,13 @@
 <?php
 
-	require_once = "../vendor/autoload.php";
+	require_once "../vendor/autoload.php";
+
+	$route = new \App\Route();
 	
 	echo "Hello World";
+ 
+	echo "<pre>";  
+		print_r($route->getUrl());
+	echo "</pre>";
 ?>
+
