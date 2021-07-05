eac42db9e853ea22d878d18efd5d3fb683eb77f9
GilbertoASJ <alvesgilberto84@gmail.com>
2021-07-05 16:23:54 -0300

    :sparkles: Updating files, creating controller

---
+++ App/Controllers/indexController.php
@@ -0,0 +1,18 @@
+<?php
+	
+	namespace App\Controllers;
+
+	class IndexController {
+
+		public function index() {
+
+			echo "Chegamos aqui na função index";
+		}
+
+		public function sobreNos() {
+
+			echo "Chegamos aqui na função sobreNos";
+		}
+	}
+
+?>
--- App/Route.php
+++ App/Route.php
@@ -4,21 +4,55 @@
 
 	class Route {
 
+		private $routes;
+
+		public function __construct() {
+
+			$this->initRoutes();
+			$this->run($this->getUrl());
+		}
+
+		public function getRoutes() {
+			return $this->routes;
+		}
+
+		public function setRoutes(array $routes) {
+			$this->routes = $routes;
+		}
+
 		public function initRoutes() {
 
-			$routes = array('home', 'sobreNos');
-
-			$routes[0] = array(
+			$routes['home'] = array(
 				'route' => '/', 
 				'controller' => 'indexController',
 				'action' => 'index',
 			);
 
-			$routes[1] = array(
+			$routes['sobreNos'] = array(
 				'route' => '/sobreNos', 
 				'controller' => 'indexController',
 				'action' => 'sobreNos',
 			);
+
+			$this->setRoutes($routes);
+		}
+
+		// Função para executar determinada ação de acordo com a url atual do client
+		public function run($url) {
+
+			foreach ($this->getRoutes() as $key => $route) {
+				
+				if($url = $route['route']) {
+
+					// Criando e instânciando a class indexController
+					$class = "App\\Controllers\\".ucfirst($route['controller']);
+
+					$controller = new $class;
+
+					$action = $route['action'];
+					$controller->$action();
+				}
+			}
 		}
 
 		public function getUrl() {
