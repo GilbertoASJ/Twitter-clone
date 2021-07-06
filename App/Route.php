15e7b532013b7bba0ce9b5a49e8224e65a44f587
GilbertoASJ <alvesgilberto84@gmail.com>
2021-07-06 15:04:02 -0300

    :sparkles: Updating file, extends abstract clas

--- App/Route.php
+++ App/Route.php
@@ -2,25 +2,13 @@
 	
 	namespace App;
 
-	class Route {
+	// Recuperando a classe abstrata para Route herda-la
+	use MF\Init\Bootstrap;
 
-		private $routes;
+	class Route extends Bootstrap {
 
-		public function __construct() {
-
-			$this->initRoutes();
-			$this->run($this->getUrl());
-		}
-
-		public function getRoutes() {
-			return $this->routes;
-		}
-
-		public function setRoutes(array $routes) {
-			$this->routes = $routes;
-		}
-
-		public function initRoutes() {
+		// Iniciando as rotas
+		protected function initRoutes() {
 
 			$routes['home'] = array(
 				'route' => '/', 
@@ -35,29 +23,6 @@
 			);
 
 			$this->setRoutes($routes);
-		}
-
-		// Função para executar determinada ação de acordo com a url atual do client
-		public function run($url) {
-
-			foreach ($this->getRoutes() as $key => $route) {
-				
-				if($url = $route['route']) {
-
-					// Criando e instânciando a class indexController
-					$class = "App\\Controllers\\".ucfirst($route['controller']);
-
-					$controller = new $class;
-
-					$action = $route['action'];
-					$controller->$action();
-				}
-			}
-		}
-
-		public function getUrl() {
-			// Utilizando uma superglobal do php para recuperar a url do client
-			return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 		}
 	}
 
