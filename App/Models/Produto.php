229ac78c460b781e693205a495f4e23bff0a70ea
GilbertoASJ <alvesgilberto84@gmail.com>
2021-07-09 16:24:01 -0300

    :sparkles: Updating file, Creating model

---
+++ App/Models/Produto.php
@@ -0,0 +1,21 @@
+<?php 
+	
+	namespace App\Models;
+
+	class Produto {
+
+		protected $db;
+
+		public function __construct(PDO $db) {
+
+			$this->db = $db;
+		}
+
+		public function getProdutos() {
+
+			return array('Mesa', 'Cadeira');
+
+			
+		}
+	}
+?>
