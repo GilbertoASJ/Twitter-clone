726b6c37ce321279bf80aa52c8f15ab2ae86a801
GilbertoASJ <alvesgilberto84@gmail.com>
2021-07-10 17:31:44 -0300

    :sparkles: Updating file, connection with db

--- App/Models/Produto.php
+++ App/Models/Produto.php
@@ -6,16 +6,16 @@
 
 		protected $db;
 
-		public function __construct(PDO $db) {
+		public function __construct(\PDO $db) {
 
 			$this->db = $db;
 		}
 
 		public function getProdutos() {
 
-			return array('Mesa', 'Cadeira');
+			$query = "select id, descricao, preco from tb_produtos";
 
-			
+			return $this->db->query($query)->fetchAll();
 		}
 	}
 ?>
