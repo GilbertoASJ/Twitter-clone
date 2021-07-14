4077431fef37ef5dfe7809d64f8778ca2c27fd9c
GilbertoASJ <alvesgilberto84@gmail.com>
2021-07-14 18:00:56 -0300

    :sparkles: Updating file

--- public/index.php
+++ public/index.php
@@ -1,7 +1,12 @@
 <?php
-
+																
 	require_once "../vendor/autoload.php";
 
 	$route = new \App\Route();
 
-?>
+?>																		
+									
+									
+									
+									
+											
