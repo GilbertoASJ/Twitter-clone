259aa2f36b4edd1883430eddcd8feac9651bc540
GilbertoASJ <alvesgilberto84@gmail.com>
2021-07-18 17:43:33 -0300

    :sparkles: Updating Controller file

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -18,6 +18,29 @@ class IndexController extends Action {
 		$this->render('inscreverse');
 	}
 
+	// Método para fazer o registro de usuário
+	public function registrar() {
+
+		// Receber os dados do formulário
+		$usuario = Container::getModel('Usuario');
+		$usuario->__set('nome', $_POST['nome']);
+		$usuario->__set('email', $_POST['email']);
+		$usuario->__set('senha', $_POST['senha']);
+
+		echo "<pre>";
+			print_r($usuario);
+		echo "</pre>";
+
+		$usuario->salvar();
+
+		// Sucesso
+
+
+		// Erro
+
+
+	}
+
 }
 
 
