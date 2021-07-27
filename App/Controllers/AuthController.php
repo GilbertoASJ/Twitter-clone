0a9bd105e4c70b23a542b3c7d24b543d268244a2
GilbertoASJ <alvesgilberto84@gmail.com>
2021-07-27 16:39:26 -0300

    :sparkles: Updating AuthController, creating logout method

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -24,13 +24,29 @@ class AuthController extends Action {
 			// Iniciando as tratativas com a superGlobal Session
 			session_start();
 
-			$_SESSION['variable'];
+			$_SESSION['id'] = $usuario->__get('id');
+			$_SESSION['nome'] = $usuario->__get('nome');
+
+			header('Location: /timeline');
 
 		} else {
 
+			// Caso a autenticação tenha falhado, o usuário será redirecionado para a página inicial com um atributo no corpo da url, fazendo que a partir desse atributo, seja adicionado uma mensagem de erro no html da página
 			header('Location: /?login=erro');
 		}
+	}
 
+	// Método para realizar o logout do usuário quando já estiver autenticado
+	public function sair() {
+
+		// Sempre ao trabalhar com o recursod e sessão precisamos utilizar o session_start,
+		session_start();
+
+		// Encerramos a sessão
+		session_destroy();
+
+		// E redirecionamos o usuário para a home
+		header('Location: /');
 	}
 }
 
