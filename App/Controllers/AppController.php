2ea03f3d6208f0556b0d1f6c6e11d9d6e51433a5
GilbertoASJ <alvesgilberto84@gmail.com>
2021-07-28 17:00:00 -0300

    :sparkles: Updating Controllers

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -22,6 +22,32 @@ class AppController extends Action {
 
 			header('Location: /?login=erro');
 		}
+	}
+
+	// Método para a manipulação do tweet
+	public function tweet() {
+
+		// Abrir a sessão
+		session_start();
+
+		// Verificar se os dados de seção existem, ou seja, verificar se o usuário fez o login
+		if($_SESSION['id'] != '' && $_SESSION['nome'] != '') {
+
+			// Instânciar o Model tweet
+			$tweet = Container::getModel('Tweet');
+
+			// Atribuindo valores aos parâmetros do Model tweet
+			$tweet->__set('tweet', $_POST['tweet']);
+			$tweet->__set('id_usuario', $_SESSION['id']);
+
+			// Salvar o tweet no banco
+			$tweet->salvar();
+
+
+		} else {
+
+			header('Location: /?login=erro');
+		}
 
 	}
 }
--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -35,7 +35,7 @@ class IndexController extends Action {
 		$usuario = Container::getModel('Usuario');
 		$usuario->__set('nome', $_POST['nome']);
 		$usuario->__set('email', $_POST['email']);
-		$usuario->__set('senha', $_POST['senha']);
+		$usuario->__set('senha', md5($_POST['senha']));
 
 		// echo "<pre>";
 		// 	print_r($usuario);
