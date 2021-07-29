5a1cd8ba99a971402261205409c05e01eca5975d
GilbertoASJ <alvesgilberto84@gmail.com>
2021-07-29 17:04:54 -0300

    :sparkles

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -8,46 +8,55 @@ use MF\Model\Container;
 
 class AppController extends Action {
 
-	public function timeline() {
+	// Método para verificar se o usuário está autenticado
+	public function validaSessao() {
 
 		// Abrir a sessão
 		session_start();
 
 		// Verificar se os dados de seção existem, ou seja, verificar se o usuário fez o login
-		if($_SESSION['id'] != '' && $_SESSION['nome'] != '') {
-
-			$this->render('timeline');
-
-		} else {
+		if(!isset($_SESSION['id']) || $_SESSION['id'] != '' || !isset($_SESSION['nome']) || $_SESSION['nome'] != '') {
 
 			header('Location: /?login=erro');
+
 		}
+	}
+
+	public function timeline() {
+
+		// Verificar se o usuário está autenticado
+		$this->validaSessao();
+
+		// Recuperação do Model tweet e dos tweet´s
+		$tweet = Container::getModel('Tweet');
+		$tweet->__set('id_usuario', $_SESSION['id']);
+
+		$tweets = $tweet->getAll();
+
+		$this->view->tweets = $tweets;         
+
+		// Renderização da página timeline
+		$this->render('timeline');
 	}
 
 	// Método para a manipulação do tweet
 	public function tweet() {
 
-		// Abrir a sessão
-		session_start();
+		// Verificar se o usuário está autenticado
+		$this->validaSessao();
 
-		// Verificar se os dados de seção existem, ou seja, verificar se o usuário fez o login
-		if($_SESSION['id'] != '' && $_SESSION['nome'] != '') {
+		// Instânciar o Model tweet
+		$tweet = Container::getModel('Tweet');
 
-			// Instânciar o Model tweet
-			$tweet = Container::getModel('Tweet');
+		// Atribuindo valores aos parâmetros do Model tweet
+		$tweet->__set('tweet', $_POST['tweet']);
+		$tweet->__set('id_usuario', $_SESSION['id']);
 
-			// Atribuindo valores aos parâmetros do Model tweet
-			$tweet->__set('tweet', $_POST['tweet']);
-			$tweet->__set('id_usuario', $_SESSION['id']);
+		// Salvar o tweet no banco
+		$tweet->salvar();
 
-			// Salvar o tweet no banco
-			$tweet->salvar();
+		header('Location: /timeline');
 
-
-		} else {
-
-			header('Location: /?login=erro');
-		}
 
 	}
 }
