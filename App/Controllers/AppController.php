ee8e9ce8a9e52196b8f359fbd89fe2adbf1b3e2a
GilbertoASJ <alvesgilberto84@gmail.com>
2021-07-30 17:43:30 -0300

    :sparkles: Updating file, creating a new method - quemSeguir

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -15,11 +15,10 @@ class AppController extends Action {
 		session_start();
 
 		// Verificar se os dados de seção existem, ou seja, verificar se o usuário fez o login
-		if(!isset($_SESSION['id']) || $_SESSION['id'] != '' || !isset($_SESSION['nome']) || $_SESSION['nome'] != '') {
+		if(!isset($_SESSION['id']) || $_SESSION['id'] == '' || !isset($_SESSION['nome']) || $_SESSION['nome'] == '') {
 
-			header('Location: /?login=erro');
-
-		}
+	      header('Location: /?login=erro');
+	    } 
 	}
 
 	public function timeline() {
@@ -59,6 +58,35 @@ class AppController extends Action {
 
 		header('Location: /timeline');
 
+
+	}
+
+	public function quemSeguir() {
+
+		// Verificar se o usuário está autenticado
+		$this->validaSessao();
+
+		$pesquisarPor = isset($_GET['pesquisarPor']) ? $_GET['pesquisarPor'] : '';
+
+		echo "<br /><br /><br /> $pesquisarPor";
+
+		if($pesquisarPor != '') {
+
+			// Instância do Model Usuario e atribuição ao 'nome', pelo termo de pesquisa($pesquisarPor)
+			$usuario = Container::getModel('Usuario');
+			$usuario->__set('nome', $pesquisarPor);
+
+			// Recuperando todas as ocorrências da pesquisa
+			$usuarios = $usuario->getAll();
+
+			echo "<pre>";
+				print_r($usuarios);
+			echo "</pre>";
+
+		}
+
+		// Ao acessar a página é feita a renderização da view quemSeguir
+		$this->render('quemSeguir');
 
 	}
 }
