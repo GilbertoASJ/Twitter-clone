2ed7a6ae917ae241278f4ba7825f7be4cf9a16be
GilbertoASJ <alvesgilberto84@gmail.com>
2021-08-01 17:09:51 -0300

    :sparkles: Updating file, creating new route

--- App/Route.php
+++ App/Route.php
@@ -62,6 +62,13 @@ class Route extends Bootstrap {
 			'action' => 'quemSeguir'
 		);
 
+		$routes['acao'] = array(
+			'route' => '/acao',
+			'controller' => 'AppController',
+			'action' => 'acao'
+		);
+
+
 
 		$this->setRoutes($routes);
 	}
